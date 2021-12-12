96bb8a48e6744c76ab7beb7cccbd83e145a4342d
Monsaw <lawalmonsaw@gmail.com>
2021-12-12 13:32:10 +0100

    changes

--- mail/urls.py
+++ mail/urls.py
@@ -3,12 +3,12 @@ from  mail import views
 app_name = 'mail'
 
 urlpatterns = [
-  path('',views.EmailList.as_view(), name = 'emails'),
+  path('',views.EmailList.as_view(), name = 'home'),
   path('add/',views.EmailCreate.as_view(), name = 'create'),
   path('edit/<pk>',views.EmailUpdate.as_view(), name = 'update'),
   path('remove/<pk>',views.EmailDelete.as_view(), name = 'delete'),
   path('send/',views.send_email, name = 'send'),
-  path('home/',views.base, name = 'home'),
+  path('emails/',views.base, name = 'emails'),
   path('about/',views.about, name = 'about'),
   
 
