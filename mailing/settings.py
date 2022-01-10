29818ceb9dc9121e0148a891db580b693f0f7f33
Monsaw <lawalmonsaw@gmail.com>
2022-01-10 17:27:47 +0100

    edited

--- mailing/settings.py
+++ mailing/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env.str("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['sendingmailsapp.herokuapp.com' ,]
+ALLOWED_HOSTS = ['sendingmailsapp.herokuapp.com' , '127.0.0.1',]
 
 
 # Application definition
@@ -82,12 +82,18 @@ WSGI_APPLICATION = 'mailing.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+"default": env.dj_db_url("DATABASE_URL")
 }
+
 
 
 # Password validation
